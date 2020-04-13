79c862a433026be508cb6985ae25b5e2a71dfaf5
unknown <fiiarkhurst@gmail.com>
2020-04-13 20:12:50 +0100

    completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,12 @@
-const covid19ImpactEstimator = (data) => data;
+import { data } from './data';
+
+const covid19ImpactEstimator = () => {
+    
+    const infected = data.reportedCases;
+    // get reported cases
+    (function getCurrentlyInfected(){
+        infected * 10
+    }(infected))
+};
 
 export default covid19ImpactEstimator;
